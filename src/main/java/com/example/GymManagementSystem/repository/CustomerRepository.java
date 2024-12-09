4a9add718cac00f4e2325a2ff412385d35db34bb
canhdinhtien <131688279+canhdinhtien@users.noreply.github.com>
2024-12-09 21:41:37 +0700

    Canhdinh

--- src/main/java/com/example/GymManagementSystem/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.GymManagementSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.GymManagementSystem.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    
-}
--- src/main/java/com/example/GymManagementSystem/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.GymManagementSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.GymManagementSystem.entity.Trainer;
-
-public interface TrainerRepository extends JpaRepository<Trainer, Integer> {
-    
-}
