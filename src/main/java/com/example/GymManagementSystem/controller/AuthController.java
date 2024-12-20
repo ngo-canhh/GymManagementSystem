efbd54d3ce7329001f562072b2b1922ff49ec0aa
Vuductai2004 <vuductai1609@gmail.com>
2024-12-20 15:30:51 +0700

    Update AuthController.java

--- src/main/java/com/example/GymManagementSystem/controller/AuthController.java
+++ src/main/java/com/example/GymManagementSystem/controller/AuthController.java
@@ -1,37 +1,47 @@
-      package com.example.GymManagementSystem.controller;
-     
+     package com.example.GymManagementSystem.controller;
 
   import com.example.GymManagementSystem.entity.User;
-      import com.example.GymManagementSystem.service.AuthService;
-       import org.springframework.beans.factory.annotation.Autowired;
-     import org.springframework.http.HttpStatus;
-      import org.springframework.http.ResponseEntity;
-     import org.springframework.web.bind.annotation.*;
+  import com.example.GymManagementSystem.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 
- @RestController
+ import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+ import org.springframework.web.bind.annotation.*;
+
+@Controller
 @RequestMapping("/auth")
 
-    public class AuthController
-      {
+ public class AuthController {
 
-    @Autowired
-  AuthService authService;
+
+     @Autowired
+    AuthService authService;
 
 
   @PostMapping("/register")
 
-    public ResponseEntity<String> register(@RequestBody User user)
-         {
+ public String register(@ModelAttribute("user") User user,
+                      @RequestParam("confirmPassword") String confirmPassword,Model model) {
+     if (!user.getPassword().equals(confirmPassword)){
+       model.addAttribute("error","Password mismatch. Please re-enter again!");
+         return "StaffViewsHtml/register";
+    }
+      
+      authService.register(user);
 
-   User u=  authService.register(user);
-   
-      if(u.getId() != null)
-
-        return new ResponseEntity<String>("user is registered", HttpStatus.CREATED);
-
-          return new ResponseEntity<>("cannot create user",HttpStatus.EXPECTATION_FAILED);
-         }
-
-  
+   return "redirect:/auth/login";
    }
- 
+
+     @GetMapping("/login")
+ public String viewLogin(){
+       return "StaffViewsHtml/login";
+   }
+
+    @GetMapping("/register")
+  public String viewRegister(){
+     return "StaffViewsHtml/register";
+   }
+
+   
+  }
