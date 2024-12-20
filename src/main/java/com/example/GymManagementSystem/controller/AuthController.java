037ad9d72f985fa5c74c181242afdd7f11a8e148
Vuductai2004 <vuductai1609@gmail.com>
2024-12-20 15:20:45 +0700

    Update AuthController.java

--- src/main/java/com/example/GymManagementSystem/controller/AuthController.java
+++ src/main/java/com/example/GymManagementSystem/controller/AuthController.java
@@ -1,15 +1,37 @@
-package com.example.gymmanagement.controller;
+      package com.example.GymManagementSystem.controller;
+     
 
-import com.example.gymmanagement.entity.User;
-import com.example.gymmanagement.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-@RestController
+  import com.example.GymManagementSystem.entity.User;
+      import com.example.GymManagementSystem.service.AuthService;
+       import org.springframework.beans.factory.annotation.Autowired;
+     import org.springframework.http.HttpStatus;
+      import org.springframework.http.ResponseEntity;
+     import org.springframework.web.bind.annotation.*;
+
+ @RestController
 @RequestMapping("/auth")
 
-public class AuthController
-   {
+    public class AuthController
+      {
 
-@Autowired
+    @Autowired
+  AuthService authService;
+
+
+  @PostMapping("/register")
+
+    public ResponseEntity<String> register(@RequestBody User user)
+         {
+
+   User u=  authService.register(user);
+   
+      if(u.getId() != null)
+
+        return new ResponseEntity<String>("user is registered", HttpStatus.CREATED);
+
+          return new ResponseEntity<>("cannot create user",HttpStatus.EXPECTATION_FAILED);
+         }
+
+  
+   }
+ 
