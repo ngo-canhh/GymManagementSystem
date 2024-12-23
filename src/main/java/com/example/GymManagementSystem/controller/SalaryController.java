d7e0b114fdb01e8375dfc9e125b82678e65b74ed
canhdinhtien <131688279+canhdinhtien@users.noreply.github.com>
2024-12-23 21:04:07 +0700

    final of final

--- src/main/java/com/example/GymManagementSystem/controller/SalaryController.java
+++ src/main/java/com/example/GymManagementSystem/controller/SalaryController.java
@@ -3,17 +3,24 @@ package com.example.GymManagementSystem.controller;
 import com.example.GymManagementSystem.DTO.SalaryDTO;
 import com.example.GymManagementSystem.service.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
+@Controller 
 @RequestMapping("/api/salaries")
 public class SalaryController {
     @Autowired
     private SalaryService salaryService;
 
+   @GetMapping("/salaryPage")  
+    public String showSalaryPage(){
+        return "admin_salary_calculation";  
+    }
+
     @PostMapping("/calculate")
+    @ResponseBody 
     public ResponseEntity<List<SalaryDTO>> calculateSalaries(@RequestParam int month, @RequestParam int year) {
         List<SalaryDTO> salaries = salaryService.calculateSalaries(month, year);
         return ResponseEntity.ok(salaries);
